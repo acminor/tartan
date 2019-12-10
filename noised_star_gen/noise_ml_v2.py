7df4ecb05dde64cb10660f764c348f5955c88894
austin <austin.chase.m@gmail.com>
2019-12-10 14:47:00 +0800

    prenoise flare implemented

--- noised_star_gen/noise_ml_v2.py
+++ noised_star_gen/noise_ml_v2.py
@@ -57,7 +57,8 @@ def main(output_dir, desc_file, plot):
             "len={},".format(len(star['samples'])) +
             "spow={:.2f},".format(star['spow']) +
             "npow={:.2f},".format(star['npow']) +
-            "snr={:.2f}".format(star['snr'])
+            "snr={:.2f},".format(star['snr']) +
+            "t0={}".format(star['t0'])
         )
 
         if plot:
@@ -173,7 +174,6 @@ def get_flare_star(A, B, C, D, E, F):
 
     return lambda t: calc(t)
 
-
 def parse_range(ran):
     LOW = 0
     HIGH = 1
@@ -245,8 +245,7 @@ def parse_noise_descriptors(descriptors):
 
     return res
 
-
-def parse_pre_noise(descriptors):
+def parse_pre_noise(descriptors, sample_rate):
     '''
     Should produce a list of functions to be
     called where each function returns the generated pre-noise.
@@ -273,7 +272,9 @@ def parse_pre_noise(descriptors):
             amplitude = parse_range(desc['amplitude'])
             tot_time = parse_range(desc['tot_time'])
             args = itertools.product(amplitude, tot_time)
-            funcs = [get_flare_star_v2(amplitude, tot_time) for (amplitude, tot_time) in args]
+            funcs = [get_flare_star_v2(amplitude, tot_time, sample_rate)
+                     for (amplitude, tot_time) in args]
+            return funcs
         else:
             print("ERROR: BAD DESCRIPTOR")
             exit(-1)
@@ -286,13 +287,13 @@ def parse_pre_noise(descriptors):
 def _gen(desc_file):
     utils.DEBUG = False
 
-    funcs = parse_noise_descriptors(desc_file['noise'])
-    #pre_noise_funcs = parse_pre_noise(desc_file['pre-noise'])
-
     sample_rate = desc_file['signal']['sample_rate']
     # NOTE make start and end length in terms of points and not seconds
     start_len = desc_file['signal']['start_len'] * sample_rate
     end_len = desc_file['signal']['end_len'] * sample_rate
+
+    funcs = parse_noise_descriptors(desc_file['noise'])
+    pre_noise_func_groups = parse_pre_noise(desc_file['pre-noise'], sample_rate)
 
     def gen_template(u0, te):
         width = int(math.ceil(te/sample_rate))
@@ -315,17 +316,39 @@ def _gen(desc_file):
     print('Amount to gen: {}'.format(tot_iter))
 
 
-    def gen_signal(f, template):
-        start = [f(t) for t in range(0, start_len, sample_rate)]
+    def gen_signal(f, pre_noise_funcs, template):
+        i = 0
 
-        middle_without_signal = [f((i + len(start))*sample_rate) for i in range(0, len(template))]
+        ran = list(range(i, i + start_len, sample_rate))
+        start = [f(t) for t in ran]
+        # NOTE: plus sample_rate on all to get the next point (not the last point used)
+        i = ran[-1] + sample_rate
+
+        pre_noise_without_signal = []
+        pre_noise = []
+        for pnf in pre_noise_funcs:
+            pre_noise += pnf()
+
+        if len(pre_noise) > 0:
+            #raise Exception("greater than 0: {}".format(pre_noise))
+            ran = list(range(i, i + len(pre_noise)*sample_rate, sample_rate))
+            pre_noise_without_signal = [f(t) for t in ran]
+            i = ran[-1] + sample_rate
+            pre_noise = \
+                [pre_noise_without_signal[i] + pre_noise[i] for i in range(0, len(pre_noise))]
+
+        ran = list(range(i, i + len(template)*sample_rate, sample_rate))
+        middle_without_signal = [f(t) for t in ran]
         middle = [middle_without_signal[i] + template[i] for i in range(0, len(template))]
+        i = ran[-1] + sample_rate
 
-        end_start = sample_rate*(len(start)+len(middle))
-        end = [f(t) for t in range(end_start, end_start + end_len, sample_rate)]
+        ran = list(range(i, i + end_len, sample_rate))
+        end = [f(t) for t in ran]
+        i = ran[-1] + sample_rate
 
         spow = signal_power(template, sample_rate)
-        npow = noise_power(start+middle_without_signal+end, sample_rate)
+        # NOTE: only considers the noise power in the part with the microlensing signal
+        npow = noise_power(middle_without_signal, sample_rate)
 
         start = np.array(start)
         # NOTE: fixed by making templates start at 0 DC
@@ -342,18 +365,20 @@ def _gen(desc_file):
         # [ ] TODO make it where we also store the signals peak point (very easy to do)
         #     will make updating code here not have to change match filter code
         # [ ] TODO fix it also in match filter
-        datum = np.array(start+middle+end)
+        datum = np.array(start+pre_noise+middle+end)
         datum = {
             'samples': datum,
             'spow': spow,
             'npow': npow,
-            'snr': spow - npow
+            'snr': spow - npow,
+            't0': int(len(start) + len(pre_noise) + len(middle)/2.0)
         }
 
         return datum
 
     data = Parallel(n_jobs=8, verbose=10)(
-        delayed(gen_signal)(f, t) for f, t in itertools.product(funcs, templates))
+        delayed(gen_signal)(f, pnfs, t) for f, pnfs, t in itertools.product(
+            funcs, pre_noise_func_groups, templates))
 
     return data
 
